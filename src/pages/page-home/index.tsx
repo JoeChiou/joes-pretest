aeca442b347da0d1684b3345f61cd6b071e0ed99
JoeChiou <iamasheep2008@gmail.com>
2022-09-29 09:49:44 +0800

    init

---
+++ src/pages/page-home/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material"
+import { makeStyles } from '@material-ui/styles';
+
+export const PageHome=()=>{
+    return (
+        <Box>home</Box>
+    )
+};
+
+const useStyle=makeStyles({
+    
+})
